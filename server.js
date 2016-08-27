06e2c96745c2fca61898ce08227b4ce2cb3791bf
MikeH <mike123.henry@gmail.com>
2016-08-26 20:02:31 -0500

    fixed server port mismatch

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ var routes = require('./controllers/controller.js');
 app.use('/', routes);
 
 // listen on port 3000
-app.listen(3001, function() {
+app.listen(3000, function() {
   console.log('App running on port 3000!');
 });
